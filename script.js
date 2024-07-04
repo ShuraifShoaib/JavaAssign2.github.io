5b51ad8236c1a036d1a835247da22a480ce5a93e
ShuraifShoaib <shuraifshoaib@gmail.com>
2024-07-04 19:00:09 +0500

    Java Assign 2

---
+++ script.js
@@ -0,0 +1,10 @@
+// Prompt the user to enter a number
+const number = prompt("Enter a number:");
+
+// Determine if the number is even or odd using the ternary operator
+const result = (number % 2 === 0) ? 'Even' : 'Odd';
+
+
+alert(`The number ${number} is ${result}`);
+
+
